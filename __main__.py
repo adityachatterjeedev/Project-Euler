fc657db4ab5bb8d0c64bec7a1d934c1bc526500e
adychats <chatterjee2307@gmail.com>
2019-10-22 15:48:20 -0400

    Modified __main__.py

--- __main__.py
+++ __main__.py
@@ -4,8 +4,9 @@ The main file for my Project Euler code.
 from problem_1 import *
 from problem_2 import *
 from problem_3 import *
+from problem_4 import *
 
-problem_dict = {1 : problem1, 2 : problem2, 3 : problem3}
+problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4}
 
 problem_int = int(input("Which problem do you want to solve? \n"))
 
