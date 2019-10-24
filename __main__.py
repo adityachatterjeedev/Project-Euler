d2f9b5f1e310a1b66c72a6d8be5dd517036f8768
adychats <chatterjee2307@gmail.com>
2019-10-24 01:59:27 -0400

    Modified __main__ to support problem 8

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 """
 The main file for my Project Euler code.
 """
-from problems_1_through_7 import *
+from problems_1_through_8 import *
 
 
 problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4, 5 : problem5, 
-                6 : problem6, 7: problem7}
+                6 : problem6, 7: problem7, 8: problem8}
 
 problem_int = int(input("Which problem do you want to solve? "))
 
