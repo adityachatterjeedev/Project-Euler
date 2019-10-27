a73371bbc392d9b2a465832fc565554336736eb8
adychats <chatterjee2307@gmail.com>
2019-10-27 13:19:50 -0400

    Gave the option of solving more than one problem at one go.

--- __main__.py
+++ __main__.py
@@ -7,6 +7,10 @@ from problems_1_through_9 import *
 problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4, 5 : problem5, 
                 6 : problem6, 7: problem7, 8: problem8, 9 : problem9}
 
-problem_int = int(input("Which problem do you want to solve? "))
+character = 'y'
+while character == 'y' or character == 'Y':    
+    problem_int = int(input("Which problem do you want to solve? "))
 
-problem_dict[problem_int]()
+    problem_dict[problem_int]()
+
+    character = input("\n Do you want to solve another problem? (y/n) ")
