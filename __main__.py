53a8b04fcff32f8a13075638c670795440c3875d
adychats <chatterjee2307@gmail.com>
2019-10-27 13:49:59 -0400

    Added support for problem 10

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 """
 The main file for my Project Euler code.
 """
-from problems_1_through_9 import *
+from problems_1_through_10 import *
 
 
 problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4, 5 : problem5, 
-                6 : problem6, 7: problem7, 8: problem8, 9 : problem9}
+                6 : problem6, 7: problem7, 8: problem8, 9 : problem9, 10 : problem10}
 
 character = 'y'
 while character == 'y' or character == 'Y':    
