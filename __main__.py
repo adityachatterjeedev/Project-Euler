0a8fc7b674ba40c671034b307fa90d8bfc278a73
adychats <chatterjee2307@gmail.com>
2019-10-29 17:20:41 -0400

    Modified __main__ slightly.

--- __main__.py
+++ __main__.py
@@ -7,10 +7,18 @@ from problems_1_through_10 import *
 problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4, 5 : problem5, 
                 6 : problem6, 7: problem7, 8: problem8, 9 : problem9, 10 : problem10}
 
-character = 'y'
-while character == 'y' or character == 'Y':    
-    problem_int = int(input("Which problem do you want to solve? "))
+def solve_problems():
+    """
+    The main function.
+    """
+    character = 'y'
+    while character == 'y' or character == 'Y':    
+        problem_int = int(input("Which problem do you want to solve? "))
 
-    problem_dict[problem_int]()
+        problem_dict[problem_int]()
 
-    character = input("\n Do you want to solve another problem? (y/n) ")
+        character = input("\n Do you want to solve another problem? (y/n) ")
+
+
+if __name__ == '__main__':
+    solve_problems()
