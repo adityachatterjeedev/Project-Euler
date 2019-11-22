f8a939d0cd4982f180ec3af0fb31dcc5d7371cce
adychats <chatterjee2307@gmail.com>
2019-11-22 04:12:30 -0500

    Implemented Problem 13

--- __main__.py
+++ __main__.py
@@ -4,10 +4,9 @@ The main file for my Project Euler code.
 from problems_1_through_10 import *
 from problems_11_through_20 import *
 
-
 problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4, 5 : problem5, 
                 6 : problem6, 7: problem7, 8: problem8, 9 : problem9, 10 : problem10,
-                11: problem11, 12: problem12}
+                11: problem11, 12: problem12, 13: problem13}
 
 def solve_problems():
     """
@@ -21,7 +20,8 @@ def solve_problems():
             try:
                 problem_dict[problem_int]()
             except KeyError:
-                print("\nI haven't implemented a solution to this problem yet, sorry. Try pulling from this repository a few days later and maybe I'll have solved the problem by then. \nThanks for your patience.")
+                print("\nI haven't implemented a solution to this problem yet, sorry. \
+Try pulling from this repository a few days later and maybe I'll have solved the problem by then. \nThanks for your patience.")
         else:
             print("\nThe problem number needs to be a positive integer!")
         character = input("\nDo you want to solve another problem? (y/n) ")
