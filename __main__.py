e9c1da2331f1886d5476e90e4fb1d09fa2d35164
adychats <chatterjee2307@gmail.com>
2019-11-28 04:52:11 -0500

    Implemented Problem 23

--- __main__.py
+++ __main__.py
@@ -4,11 +4,13 @@ The main file for my Project Euler code.
 from problems_1_through_10 import *
 from problems_11_through_20 import *
 from problems_21_through_30 import *
+from problem_23 import *
+
 problem_dict = {1 : problem1, 2 : problem2, 3 : problem3, 4 : problem4, 5 : problem5, 
                 6 : problem6, 7: problem7, 8: problem8, 9 : problem9, 10 : problem10,
                 11: problem11, 12: problem12, 13: problem13, 14: problem14, 15: problem15,
                 16: problem16, 17: problem17, 18: problem18, 19: problem19, 20: problem20,
-                21: problem21, 22: problem22}
+                21: problem21, 22: problem22, 23: problem23}
 
 def solve_problems():
     """
--- problems_21_through_30.py
+++ problems_21_through_30.py
@@ -19,19 +19,6 @@ def problem21():
 
     Evaluate the sum of all the amicable numbers under 10000.
     """
-    def sum_factors(n: int):
-        """
-        Finds the sum of proper divisors of n.
-        """
-        factor_list = []
-        for i in range(1, int(n ** 0.5 + 1)):
-            if n % i == 0:
-                if (n/i == i):
-                    factor_list.append(i)
-                else:
-                    factor_list.append(i)
-                    factor_list.append(n/i)
-        return int(sum(factor_list) - n)
 
     factor_sum_dict = {}
 
@@ -92,4 +79,71 @@ def problem22():
     print(problem22.__doc__)
     print("    Answer: The total of all name scores in the file is {score}.".format(score = total_score))
 
+#Problem 23
+def problem23():
+    """
+    Project Euler Problem 23
+    https://projecteuler.net/problem=23
+
+    The problem:
+
+    A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. 
+    For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+    A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+    As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of 
+    two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as 
+    the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that
+    the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+    Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+    """
+    abundant_numbers_list = []
+    for i in range(1, 20162):
+        if sum_factors(i) > i:
+            abundant_numbers_list.append(i)
+
+# we now have a list that is sorted, making our job a bit easier.
     
+    def is_sum_of_abundant_numbers(n: int):
+        """
+        Returns True if n is a sum of 2 abundant numbers, False otherwise.
+        """
+        i = 0
+        temp = abundant_numbers_list[i]
+        while temp < n:
+            try:
+                if sum_factors(n - temp) > (n - temp):
+                    return True
+                i += 1
+                temp = abundant_numbers_list[i]
+            except IndexError:
+                return False
+        else:
+            return False
+
+    total_sum = 0
+    for j in range(1, 20162):
+        if not is_sum_of_abundant_numbers(j):
+            total_sum += j
+
+    print(problem23.__doc__)
+    print("    Answer: The sum of all positive integers that cannot be written as a sum of abundant numbers \
+is {num}.".format(num = total_sum))
+#------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------- Helper functions ---------------------------------------------------------------
+
+def sum_factors(n: int):
+    """
+    Finds the sum of proper divisors of n.
+    """
+    factor_list = []
+    for i in range(1, int(n ** 0.5 + 1)):
+        if n % i == 0:
+            if (n/i == i):
+                factor_list.append(i)
+            else:
+                factor_list.append(i)
+                factor_list.append(n/i)
+    return int(sum(factor_list) - n)
